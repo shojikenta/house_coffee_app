620df60931dc04cbf0b70b29a599b656a9db974a
shojikenta <shojikenta4@gmail.com>
2020-06-09 23:59:11 +0900

    staticpages routesの変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/about'
-  get 'static_pages/terms'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'static_pages#home'
+  get :about,        to: 'static_pages#about'
+  get :use_of_terms, to: 'static_pages#terms'
 end
