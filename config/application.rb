dc44fbea3c9ba3b59c9b62213d8690377397af64
shojikenta <shojikenta4@gmail.com>
2020-06-09 23:47:00 +0900

    generate static pages

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,10 @@ module HouseCoffee
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
+    config.generators do |g|
+      g.assets false          # CSS, JavaScriptファイルは生成しない
+      g.test_framework false  # testファイルは生成しない
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
