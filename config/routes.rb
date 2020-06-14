7424c3a9463a07553fb8fd003208683f33ee0a2c
shojikenta <shojikenta4@gmail.com>
2020-06-14 17:35:02 +0900

    usersコントローラモデルの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/new'
+  get 'users/show'
+  get 'users/index'
   root 'static_pages#home'
   get :about,        to: 'static_pages#about'
   get :use_of_terms, to: 'static_pages#terms'
+  get :signup,       to: 'users#new'
+  resources :users
 end
