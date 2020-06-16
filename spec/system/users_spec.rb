a01953b9a8847a86e3dc566a31f86a38d37ebe37
shojikenta <shojikenta4@gmail.com>
2020-06-16 21:29:36 +0900

    ユーザー登録ページ作成

---
+++ spec/system/users_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe "Users", type: :system do
+  describe "ユーザー登録ページ" do
+    before do
+      visit signup_path
+    end
+
+    context "ページレイアウト" do
+      it "「ユーザー登録」の文字列が存在することを確認" do
+        expect(page).to have_content 'ユーザー登録'
+      end
+
+      it "正しいタイトルが表示されることを確認" do
+        expect(page).to have_title full_title('ユーザー登録')
+      end
+    end
+  end
+end
