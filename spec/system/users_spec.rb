bda0abea6542c68fb5034e6b55dcf1f9d68e2095
shojikenta <shojikenta4@gmail.com>
2020-06-20 11:21:47 +0900

    ユーザー更新・一覧・削除機能の実装

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -2,6 +2,39 @@ require 'rails_helper'
 
 RSpec.describe "Users", type: :system do
   let!(:user) { create(:user) }
+  let!(:admin_user) { create(:user, :admin) }
+
+  describe "ユーザー一覧ページ" do
+    context "管理者ユーザーの場合" do
+      it "ぺージネーション、自分以外のユーザーの削除ボタンが表示されること" do
+        create_list(:user, 30)
+        login_for_system(admin_user)
+        visit users_path
+        expect(page).to have_css "div.pagination"
+        User.paginate(page: 1).each do |u|
+          expect(page).to have_link u.name, href: user_path(u)
+          expect(page).to have_content "#{u.name} | 削除" unless u == admin_user
+        end
+      end
+    end
+
+    context "管理者ユーザー以外の場合" do
+      it "ぺージネーション、自分のアカウントのみ削除ボタンが表示されること" do
+        create_list(:user, 30)
+        login_for_system(user)
+        visit users_path
+        expect(page).to have_css "div.pagination"
+        User.paginate(page: 1).each do |u|
+          expect(page).to have_link u.name, href: user_path(u)
+          if u == user
+            expect(page).to have_content "#{u.name} | 削除"
+          else
+            expect(page).not_to have_content "#{u.name} | 削除"
+          end
+        end
+      end
+    end
+  end
 
   describe "ユーザー登録ページ" do
     before do
@@ -40,9 +73,53 @@ RSpec.describe "Users", type: :system do
     end
   end
 
+  describe "プロフィール編集ページ" do
+    before do
+      login_for_system(user)
+      visit user_path(user)
+      click_link "プロフィール編集"
+    end
+
+    context "ページレイアウト" do
+      it "正しいタイトルが表示されることを確認" do
+        expect(page).to have_title full_title('プロフィール編集')
+      end
+    end
+
+    it "有効なプロフィール更新を行うと、更新成功のフラッシュが表示されること" do
+      fill_in "ユーザー名", with: "Edit Example User"
+      fill_in "メールアドレス", with: "edit-user@example.com"
+      fill_in "自己紹介", with: "編集：初めまして"
+      click_button "更新する"
+      expect(page).to have_content "プロフィールが更新されました！"
+      expect(user.reload.name).to eq "Edit Example User"
+      expect(user.reload.email).to eq "edit-user@example.com"
+      expect(user.reload.introduction).to eq "編集：初めまして"
+    end
+
+    it "無効なプロフィール更新をしようとすると、適切なエラーメッセージが表示されること" do
+      fill_in "ユーザー名", with: ""
+      fill_in "メールアドレス", with: ""
+      click_button "更新する"
+      expect(page).to have_content 'ユーザー名を入力してください'
+      expect(page).to have_content 'メールアドレスを入力してください'
+      expect(page).to have_content 'メールアドレスは不正な値です'
+      expect(user.reload.email).not_to eq ""
+    end
+
+    context "アカウント削除処理", js: true do
+      it "正しく削除できること" do
+        click_link "アカウントを削除する"
+        page.driver.browser.switch_to.alert.accept
+        expect(page).to have_content "自分のアカウントを削除しました"
+      end
+    end
+  end
+
   describe "プロフィールページ" do
     context "ページレイアウト" do
       before do
+        login_for_system(user)
         visit user_path(user)
       end
 
@@ -57,6 +134,10 @@ RSpec.describe "Users", type: :system do
       it "ユーザー情報が表示されることを確認" do
         expect(page).to have_content user.name
         expect(page).to have_content user.introduction
+      end
+
+      it "プロフィール編集ページへのリンクが表示されていることを確認" do
+        expect(page).to have_link 'プロフィール編集', href: edit_user_path(user)
       end
     end
   end
