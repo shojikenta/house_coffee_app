cb0e672d708290fdeda620a45f92fd041e9e2078
shojikenta <shojikenta4@gmail.com>
2020-07-06 10:06:32 +0900

    通知機能の作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_06_002834) do
+ActiveRecord::Schema.define(version: 2020_07_06_003942) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "drink_id"
@@ -77,6 +77,7 @@ ActiveRecord::Schema.define(version: 2020_07_06_002834) do
     t.string "password_digest"
     t.string "remember_digest"
     t.boolean "admin", default: false
+    t.boolean "notification", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
