ccacbfc011caf335737ad2ce61733dd3c09e6f53
shojikenta <shojikenta4@gmail.com>
2020-07-11 19:37:50 +0900

    aws s3の設定

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -1,6 +1,12 @@
 class PictureUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
+  if Rails.env.production?
+    storage :fog
+  else
+    storage :file
+  end
+
   storage :file
   # storage :fog
 
